9cf32d3304db4d7c240a828efd1ab2b5b9f2ae93
Dan Bikle <dan@getworksimple.com>
2012-12-03 23:26:59 +0000

    c1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ey11::Application.config.secret_token = 'babb937850da72cedec123ff3608fccdf59d440f553acf1a3f90b957a8762a8aa4c0d95cc439d51184e4dd6f98d2b05265f51bfd1f7599cc32ee1e566203f2c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ey11::Application.config.session_store :cookie_store, key: '_ey11_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ey11::Application.config.session_store :active_record_store
